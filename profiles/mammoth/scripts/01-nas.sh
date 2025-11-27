d85caa9c312ddd756ad0b9ec4f4e9fb4a32f5d62
Peter Steiwer <psteiwer@gmail.com>
2025-11-26 22:39:34 -0500

    use nfs instead

--- profiles/mammoth/scripts/01-nas.sh
+++ profiles/mammoth/scripts/01-nas.sh
@@ -2,22 +2,23 @@
 set -euo pipefail
 
 mkdir -p /gitea/db
-SERVER_SHARE="//ds923.local/gitea/db"
+NAS_HOST="ds923.local"
+REMOTE_PATH="/volume1/gitea/db"
 MOUNT_POINT="/gitea/db"
-USER_OPTS="uid=999,gid=999,file_mode=0700,dir_mode=0700"        # everything after credentials=… (you can omit credentials if you put it here)
-bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-smb-mounts.sh" "$SERVER_SHARE" "$MOUNT_POINT" "$USER_OPTS"
+bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-nfs-mounts.sh" "$NAS_HOST" "$REMOTE_PATH" "$MOUNT_POINT"
+
 
 mkdir -p /gitea/data
-SERVER_SHARE="//ds923.local/gitea/data"
-MOUNT_POINT="/gitea/data"
-USER_OPTS="uid=1001,gid=1001,file_mode=0775,dir_mode=0775"
-bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-smb-mounts.sh" "$SERVER_SHARE" "$MOUNT_POINT" "$USER_OPTS"
+NAS_HOST="ds923.local"
+REMOTE_PATH="/volume1/gitea/db"
+MOUNT_POINT="/gitea/db"
+bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-nfs-mounts.sh" "$NAS_HOST" "$REMOTE_PATH" "$MOUNT_POINT"
 
 mkdir -p /network/TransactionDataBackup
-SERVER_SHARE="//ds923.local/backups/Apps/TransactionData"
+NAS_HOST="ds923.local"
+REMOTE_PATH="/volume1/backups/Apps/TransactionData"
 MOUNT_POINT="/network/TransactionDataBackup"
-USER_OPTS="uid=999,file_mode=0700,dir_mode=0700"
-bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-smb-mounts.sh" "$SERVER_SHARE" "$MOUNT_POINT" "$USER_OPTS"
+bash "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../../shared/scripts/setup-nfs-mounts.sh" "$NAS_HOST" "$REMOTE_PATH" "$MOUNT_POINT"
 
 
 # #//ds923.local/services/ddnsupdate/logs /services/ddnsupdate/logs cifs credentials=/root/.smbcredentials,iocharset=utf8,file_mode=0600,dir_mode=0700 0 0
